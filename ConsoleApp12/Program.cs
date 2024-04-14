4476f8dd5a9a863ce3fce2690b6623cc0a984540
Bestoloc <hinz8amq17x7@mail.ru>
2024-04-14 16:06:16 +0300

    Done

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Math;
 
 namespace ConsoleApp12
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -16,48 +17,61 @@ namespace ConsoleApp12
 
             double Ravny0 = 0;
             double j = 0;
-            int i = 0;
+            
             string Nomer = "";
             int k = 1;
             double Proizved = 1;
+            Random rnd = new();
 
-            Random rnd = new Random();
-
-            while (i <= 6)
+            // Ввод массива
+            for (int i = 0; i <= 6; i++)
             {
                 z[i] = rnd.Next(0, 5);
+            }
 
-                j = z[i] / (-6);
+            
 
+            // Количество элементов массива z[i], равных 0, и номера таких элементов
+            for (int i = 0; i <= 6; i++)
+            {
                 if (z[i] == 0)
                 {
                     Ravny0++;
                     Nomer += $"{i + 1}";
                 }
-                Console.WriteLine($"z[i] = {z[i]}, f[i] = {j}");
-
-                
-                i++;
-
-                if (j < 0 && k % 2 != 0)
-                {
-                    Proizved *= j;
-                }
-                k++;
+                Console.Write($"z[{i + 1}] = {z[i]}\n");
             }
-
+            Console.WriteLine();
             if (Ravny0 != 0)
             {
-
-                
-                Console.WriteLine($"Кол-во элементов равных нулю - {Ravny0}, номера - {Nomer}");
+                Console.Write($"Кол-во элементов равных нулю - {Ravny0}, номера таких элементов - {Nomer}\n"); 
             }
             else
             {
                 Console.WriteLine("Элементов равных нулю нет!");
             }
             Console.WriteLine();
+
+            // Элементы массива f[i], i = 1..7 по формуле ...
+            for (int i = 0; i <= 6; i++)
+            {
+                j = z[i] / (-6);
+                Console.Write($"f[{i + 1}] = {j} \n");
+
+            }
+
+
+            // произведение отрицательных элементов массива f[i], расположенных на нечетных позициях
+
+            for (int i = 0; i <= 6; i++)
+            {
+                if (j < 0 && k % 2 != 0)
+                {
+                    Proizved *= j;
+                }
+            }
             Console.WriteLine($"\nПроизведение - {Proizved}\n");
+
         }
     }
 }
